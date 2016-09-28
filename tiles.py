db08674e19be218d99525a73074126125dba8b60
Ruslan Osipov <ruslan@rosipov.com>
2016-09-28 16:13:40 -0700

    Move tile logic to tiles.py

---
+++ tiles.py
@@ -0,0 +1,27 @@
+import collections
+
+TILE_TYPES = {
+    'door': '+',
+    'floor': '.',
+    'wall': '#',
+
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0',
+    }
+
+TILE_TYPE_SELECT = collections.OrderedDict(sorted(zip(TILE_TYPES.keys(),
+        ["{} {}".format(char, name) for name, char in TILE_TYPES.items()])))
+
+
+class Tile(object):
+
+    def __init__(self, tile_type):
+        self.char = TILE_TYPES[tile_type]
